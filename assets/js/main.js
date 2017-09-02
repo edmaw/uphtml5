901cc6249ac6221e70804d1d8fe44095c5a91db4
edmaw <9554843+edmaw@users.noreply.github.com>
2017-09-02 19:23:43 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,9 +39,9 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/bg01.jpg': 'center',
-							'images/bg02.jpg': 'center',
-							'images/bg03.jpg': 'center'
+							'<?php echo get_stylesheet_directory_uri()?>/images/bg01.jpg': 'center',
+							'<?php echo get_stylesheet_directory_uri()?>/images/bg02.jpg': 'center',
+							'<?php echo get_stylesheet_directory_uri()?>/images/bg03.jpg': 'center'
 						},
 
 					// Delay.
